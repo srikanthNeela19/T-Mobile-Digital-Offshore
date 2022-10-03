2d925ff32ca2a5dfcd10ef3b5f27c54a860c896d
Deepankar Gupta <deepankar.gupta@globallogic.com>
2022-10-03 15:46:11 +0530

    project init

---
+++ app/src/main/java/com/tmobile/ecom/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tmobile.ecom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
